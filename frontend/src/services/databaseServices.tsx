bb98c488008a6e7a2d26d5d322dfccf62dfe20a1
Sean Humm <sean.humm@gmail.com>
2022-06-05 23:35:42 -0700

    store/retrieveProfiles

--- frontend/src/services/databaseServices.tsx
+++ frontend/src/services/databaseServices.tsx
@@ -1,9 +1,17 @@
 import { databaseClient } from "./HttpService";
 
-export async function storeCharacters(character) {
+export function storeCharacters(character) {
     return databaseClient.post("/character", {id: character.id, name: character.name, avatar: character.avatar, searchId: character.searchId})
 }
 
-export async function retreiveCharacters(searchId) {
+export function retreiveCharacters(searchId) {
     return databaseClient.post("/getCharacters", {searchId: searchId})
 }  
+
+export function storeProfile(profile) {
+    return databaseClient.post("/character", {id: profile.id, titleId: profile.titleId, job: profile.job, level: profile.level, portrait: profile.portrait})
+}
+
+export function retreiveProfile(searchId) {
+    return databaseClient.post("/getProfiles", {searchId: searchId})
+}
