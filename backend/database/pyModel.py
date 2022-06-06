ea757e9f6b47ebab2984fcef4460e752be2c5efc
Sean Humm <sean.humm@gmail.com>
2022-06-05 23:34:28 -0700

    update for storing profile info

--- backend/database/pyModel.py
+++ backend/database/pyModel.py
@@ -10,17 +10,21 @@ class model(Model):
         try:
             cursor.execute("select count(rowid) from characters")
         except sqlite3.OperationalError:
-            cursor.execute("create table characters (id text, name text, avatar text, searchId int)")
+            cursor.execute("create table characters (id text, name text, titleId int, job text, level int, avatar text, portrait text, searchId int)")
         cursor.close()
     
     def selectResults(self, searchId):
         print("mySearchID: ", searchId)
         connection = sqlite3.connect(DB_FILE)
         cursor = connection.cursor()
-        result = cursor.execute("SELECT * FROM characters WHERE searchId = ?", (searchId,)).fetchall()
-        #print(result)
-        #result = cursor.fetchall()
+        result = cursor.execute("SELECT name, avatar FROM characters WHERE searchId = ?", (searchId,)).fetchall()
         return result
+
+    def selectProfile(self, searchId):
+        #connection = sqlite3.connect(DB_FILE)
+        #cursor = connection.cursor()
+        #result = cursor.execute("SELECT name, portrait FROM characters WHERE searchID = ?", (searchId,)).fetchall()
+        return 'OK'
 
     def insert(self, id, name, avatar, searchId):
         params = {'id':id, 'name':name, 'avatar':avatar, 'searchId':searchId}
@@ -30,3 +34,12 @@ class model(Model):
         connection.commit()
         cursor.close()
         return True
+
+    def insertProfile(self, id, title, job, level, portrait):
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("UPDATE characters SET titleId = ?, job = ?, level = ?, portrait = ? WHERE id = ?", (title, job, level, portrait, id))
+        connection.commit()
+        cursor.close()
+        return True
+
