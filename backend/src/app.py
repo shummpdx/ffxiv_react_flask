97d159482b79965824484222fd469478c90611cf
Sean Humm <sean.humm@gmail.com>
2022-06-09 20:58:08 -0700

    split routes into character and profile

--- backend/src/app.py
+++ backend/src/app.py
@@ -15,14 +15,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin(origin='localhost')
 def character():
     data = request.get_json()
-    try:
-        model.insertProfile(data['id'], data['titleId'], data['job'], data['level'], data['portrait'])
-    except Exception as e:
-        print("error", e)
-    try:
-        model.insert(data['id'], data['name'], data['avatar'], data['searchId'])
-    except Exception as e:
-        print(e)
+    model.insert(data['id'], data['name'], data['avatar'], data['searchId'])
+    return 'OK'
+
+@app.route("/profile", methods=['POST'])
+@cross_origin(origin='localhost')
+def profile():
+    data = request.get_json()
+    model.insertProfile(data['id'], data['titleId'], data['job'], data['level'], data['portrait'])
     return 'OK'
 
 @app.route("/getCharacters", methods=['POST'])
@@ -37,11 +37,10 @@ def getCharacter():
 @cross_origin(origin='localhost')
 def getProfile():
     data = request.get_json()
-    entries = [dict(name=row[0], avatar=row[1]) for row in model.selectProfile(data['searchId'])]
-    #print(entries)
-    #newEntries = json.dumps(entries)
-    #return newEntries
-    return 'OK'
+    entries = [dict(name=row[1], portrait=row[6]) for row in model.selectProfile(data['searchId'])]
+    newEntries = json.dumps(entries)
+    print("newEntries:", newEntries)
+    return newEntries
      
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
