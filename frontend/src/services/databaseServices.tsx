0d9a3ebd6f17f9b84bf1edaa74a1114aad155762
Sean Humm <sean.humm@gmail.com>
2022-06-09 21:00:27 -0700

    formatting

--- frontend/src/services/databaseServices.tsx
+++ frontend/src/services/databaseServices.tsx
@@ -1,17 +1,36 @@
 import { databaseClient } from "./HttpService";
 
 export function storeCharacters(character) {
-    return databaseClient.post("/character", {id: character.id, name: character.name, avatar: character.avatar, searchId: character.searchId})
+    return databaseClient.post("/character", 
+    {
+        id: character.id, 
+        name: character.name, 
+        avatar: character.avatar, 
+        searchId: character.searchId
+    })
 }
 
 export function retreiveCharacters(searchId) {
-    return databaseClient.post("/getCharacters", {searchId: searchId})
+    return databaseClient.post("/getCharacters", 
+    {
+        searchId: searchId
+    })
 }  
 
 export function storeProfile(profile) {
-    return databaseClient.post("/character", {id: profile.id, titleId: profile.titleId, job: profile.job, level: profile.level, portrait: profile.portrait})
+    return databaseClient.post("/profile", 
+    {
+        id: profile.id, 
+        titleId: profile.titleId, 
+        job: profile.job, 
+        level: profile.level, 
+        portrait: profile.portrait
+    })
 }
 
 export function retreiveProfile(searchId) {
-    return databaseClient.post("/getProfiles", {searchId: searchId})
+    return databaseClient.post("/getProfiles", 
+    {
+        searchId: searchId
+    })
 }
